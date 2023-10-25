b27b895cbeae5e8a4783160e7881376a891e00df
Björn Tenje Persson <bjorn@tenje.se>
2023-10-25 08:14:14 +0200

    Merged bluetooth scripts

--- polybar/scripts/bluetooth.sh
+++ polybar/scripts/bluetooth.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
-bluetoothctl | while read -r; do
-    if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
-        if bluetoothctl show|grep -q "Powered: yes"; then
-            if [ "$(bluetoothctl devices Connected|wc -l)" = "0" ]; then
-                echo ON
-            else 
-                echo CONNECTED
-            fi
-        else 
-            echo OFF
-        fi
-    else
-        echo 'SERVICE DISABLED'
-    fi
-done
+
+if [ "$1" = "status" ]; then
+	if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
+		if bluetoothctl show | grep -q "Powered: yes"; then
+			if [ "$(bluetoothctl devices Connected | wc -l)" = "0" ]; then
+				echo ON
+			else
+				echo CONNECTED
+			fi
+		else
+			echo OFF
+		fi
+	else
+		echo 'SERVICE DISABLED'
+	fi
+	sleep 1
+else
+	if bluetoothctl show | grep -q "Powered: no"; then
+		bluetoothctl power on
+	else
+		bluetoothctl power off
+	fi
+fi
