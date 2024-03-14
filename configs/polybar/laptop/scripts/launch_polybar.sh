566a65e00422ad849b18aea2cb284d1b38184734
Björn Tenje Persson <bjorn@tenje.se>
2024-03-14 17:25:50 +0100

    Desktop and laptop polybar now use same

--- configs/polybar/laptop/scripts/launch_polybar.sh
+++ configs/polybar/laptop/scripts/launch_polybar.sh
@@ -1,2 +1,12 @@
 #/bin/bash
-polybar --reload mybar &
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		bp="none"
+    if [ $(xrandr --query | grep "$m"| cut -d" " -f3) = "primary" ]; then
+      bp="right"
+    fi
+		MONITOR=$m BARPOS=$bp polybar --reload mybar &
+	done
+else
+	polybar --reload mybar &
+fi
