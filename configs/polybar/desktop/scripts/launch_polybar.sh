59c8ae911f03b9623a3579f3096dbe951fd5b9f2
Björn Tenje Persson <bjorn@tenje.se>
2024-03-14 16:56:24 +0100

    Edited polybar launcher to work on multiple screens

--- configs/polybar/desktop/scripts/launch_polybar.sh
+++ configs/polybar/desktop/scripts/launch_polybar.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 if type "xrandr"; then
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		bp="right"
-		if (($m == "DP-2")); then
-			bp="none"
-		fi
+		bp="none"
+    if [ $(xrandr --query | grep "$m"| cut -d" " -f3) = "primary" ]; then
+      bp="right"
+    fi
 		MONITOR=$m BARPOS=$bp polybar --reload mybar &
 	done
 else
